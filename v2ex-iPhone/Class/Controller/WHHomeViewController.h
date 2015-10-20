4fcde0e58bb2af4ac336bd13b3d45d11c6facd4e
haohaow <wuhao.wu@icloud.com>
2015-10-20 12:28:57 +0800

    commit changes

---
+++ v2ex-iPhone/Class/Controller/WHHomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  WHHomeViewController.h
+//  v2ex-iPhone
+//
+//  Created by hhw on 15/10/20.
+//  Copyright (c) 2015年 wuhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WHHomeViewController : UIViewController
+
+@end
---
+++ v2ex-iPhone/Class/Controller/WHHomeViewController.m
@@ -0,0 +1,13 @@
+//
+//  WHHomeViewController.m
+//  v2ex-iPhone
+//
+//  Created by hhw on 15/10/20.
+//  Copyright (c) 2015年 wuhao. All rights reserved.
+//
+
+#import "WHHomeViewController.h"
+
+@implementation WHHomeViewController
+
+@end
