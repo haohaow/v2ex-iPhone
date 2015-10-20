8c600c4988460b3492919f174ab39fc67f8a2a6e
haohaow <wuhao.wu@icloud.com>
2015-10-20 12:27:55 +0800

    commit changes

---
+++ v2ex-iPhone/Class/Manager/WHDataManager.h
@@ -0,0 +1,13 @@
+//
+//  WHDataManager.h
+//  v2ex-iPhone
+//
+//  Created by hhw on 15/10/20.
+//  Copyright (c) 2015年 wuhao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WHDataManager : NSObject
+
+@end
---
+++ v2ex-iPhone/Class/Manager/WHDataManager.m
@@ -0,0 +1,13 @@
+//
+//  WHDataManager.m
+//  v2ex-iPhone
+//
+//  Created by hhw on 15/10/20.
+//  Copyright (c) 2015年 wuhao. All rights reserved.
+//
+
+#import "WHDataManager.h"
+
+@implementation WHDataManager
+
+@end
