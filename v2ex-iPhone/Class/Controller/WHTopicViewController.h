d14bdd5e58e158976e62d259d977f81b7e640f45
haohaow <wuhao.wu@icloud.com>
2015-10-27 10:10:17 +0800

    commit changes

---
+++ v2ex-iPhone/Class/Controller/WHTopicViewController.h
@@ -0,0 +1,13 @@
+//
+//  WHHotViewController.h
+//  v2ex-iPhone
+//
+//  Created by hhw on 15/10/21.
+//  Copyright (c) 2015年 wuhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WHTopicViewController : UITableViewController
+
+@end
